065ef7fbd4d78584a228c955ba808a343c88aa38
auntim <hossennull@gmail.com>
2025-05-06 22:25:15 +0600

    add category wise and best seller page in ui

---
+++ server/controllers/productController.js
@@ -0,0 +1,22 @@
+
+
+// add product : /api/product/add
+export const addProduct = async (req, res) => {
+
+}
+
+
+// get products : /api/product/list
+export const productList = async (req, res) => {
+
+}
+
+// get product by id : /api/product/id
+export const getProductById = async (req, res) => {
+
+}
+
+// change product stock : /api/product/stock
+export const changeStock = async (req, res) => {
+
+}
