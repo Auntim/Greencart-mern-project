d4b1e0c06d5ee9659dd85c3638d2d97e578c5d0d
auntim <hossennull@gmail.com>
2025-05-07 00:29:30 +0600

    add addtoCart and order funtionality

---
+++ server/controllers/cardController.js
@@ -0,0 +1,21 @@
+
+import User from "../models/User.js";
+
+
+// update user cartData : /api/cart/update
+export const updateCart = async (req, res) => {
+    try {
+        const { userId, cartItems } = req.body;
+        await User.findByIdAndUpdate(userId, { cartItems: cartItems });
+        res.json({
+            success: true,
+            message: "Cart updated successfully",
+        })
+    } catch (error) {
+        console.log(error.message);
+        return res.json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
